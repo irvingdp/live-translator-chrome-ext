5b014e2dbf9feab0c92b9bb13eb00569ae1de403
ivan <ivan.chang@rybit.io>
2026-08-05 14:56:50 +0800

    chore: scaffold bilingual captions extension

---
+++ wxt.config.ts
@@ -0,0 +1,17 @@
+import { defineConfig } from 'wxt';
+
+export default defineConfig({
+  modules: ['@wxt-dev/module-react'],
+  manifest: {
+    name: '雙語即時字幕翻譯',
+    description: '擷取目前分頁音訊並顯示可調整大小的雙語即時字幕。',
+    minimum_chrome_version: '116',
+    permissions: ['activeTab', 'offscreen', 'scripting', 'storage', 'tabCapture'],
+    host_permissions: [
+      'https://api.deepgram.com/*',
+      'https://api.deepl.com/*',
+      'https://api-free.deepl.com/*'
+    ]
+  }
+});
+
